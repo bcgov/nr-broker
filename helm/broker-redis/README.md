ba5d3c5777eda0e390f820d868352a6c64ab43c1
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2022-09-22 13:56:56 -0700

    feat: read redis connection values from env

--- helm/broker-redis/README.md
+++ helm/broker-redis/README.md
@@ -1,5 +1,8 @@
+# Redis installation
 
-helm install redis -f values.yaml bitnami/redis
+See: https://github.com/bitnami/charts/tree/master/bitnami/redis/#installing-the-chart
+
+The values.yaml file is necessary to correctly install on OpenShift.
 
 ```
 helm install redis -f values.yaml bitnami/redis
--- src/persistence/persistence.module.ts
+++ src/persistence/persistence.module.ts
@@ -5,7 +5,18 @@ import { PersistenceService } from './persistence.service';
 const redisFactory = {
   provide: 'REDIS_CLIENT',
   useFactory: async () => {
-    const client = createClient();
+    const host = process.env.REDIS_HOST ? process.env.REDIS_HOST : 'localhost';
+    const port = process.env.REDIS_PORT ? process.env.REDIS_PORT : '6379';
+    const username = process.env.REDIS_USER ? process.env.REDIS_USER : '';
+    const password = process.env.REDIS_PASSWORD
+      ? `:${process.env.REDIS_PASSWORD}`
+      : '';
+    const url = `redis://${username}${password}${
+      username.length > 0 || password.length > 0 ? '@' : ''
+    }${host}:${port}`;
+    const client = createClient({
+      url,
+    });
     client.on('error', (err) => {
       logger.error('Redis client error');
       logger.error(err);
