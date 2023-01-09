cdafff0be5028c74d50831acabcee6a0779a9acc
Matthew Bystedt <mbystedt@gmail.com>
2023-01-09 08:39:18 -0800

    Feat/token improvements (#37)
    * feat: convert seconds to timestamp for JWT audit
    * feat: increase default to 90 days
    * feat: add client_id
    * feat: improve allocation scripts
    * feat: wrap using prod vault

--- scripts/role-finder.sh
+++ scripts/role-finder.sh
@@ -11,7 +11,7 @@ if [ -z "$1" ]
 fi
 
 if [ -n "$1" ]
-  then
+then
   VAULT_ADDR="https://vault-iit-$1.apps.silver.devops.gov.bc.ca"
   if [ "prod" = $1 ]
   then
@@ -22,11 +22,18 @@ if [ -n "$1" ]
   TEST_ROLE_ID=$(VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=$VAULT_TOKEN vault read -format json auth/vs_apps_approle/role/$2_$3_test/role-id 2> /dev/null | jq -r '.data.role_id')
   PROD_ROLE_ID=$(VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=$VAULT_TOKEN vault read -format json auth/vs_apps_approle/role/$2_$3_prod/role-id 2> /dev/null | jq -r '.data.role_id')
 
+  if [ "prod" != $1 ]
+  then
+    VAULT_ADDR="https://vault-iit.apps.silver.devops.gov.bc.ca"
+    VAULT_TOKEN=$(VAULT_ADDR=$VAULT_ADDR vault login -method=oidc -format json -no-store | jq -r '.auth.client_token')
+  fi
+
   echo "$2 : $3 - Role Id"
   echo "dev: $DEV_ROLE_ID"
   echo "test: $TEST_ROLE_ID"
   echo "prod: $PROD_ROLE_ID"
-  VAULT_WRAP_JSON=$(echo "{\"app\": \"$2 : $3\", \"dev\": \"$DEV_ROLE_ID\", \"test\": \"$TEST_ROLE_ID\", \"prod\": \"$PROD_ROLE_ID\"}" | \
-    VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=$VAULT_TOKEN vault write -format json /sys/wrapping/wrap -)
+  echo "Wrapped token:"
+  VAULT_WRAP_JSON=$(echo "{\"service\": \"$2 : $3\", \"dev\": \"$DEV_ROLE_ID\", \"test\": \"$TEST_ROLE_ID\", \"prod\": \"$PROD_ROLE_ID\", \"vault_env\": \"$1\"}" | \
+    VAULT_ADDR=$VAULT_ADDR VAULT_TOKEN=$VAULT_TOKEN vault write -format json /sys/wrapping/wrap - | jq '.wrap_info.token')
   echo $VAULT_WRAP_JSON | jq '.'
 fi
