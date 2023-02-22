45b7a6472fcb69121d9b65087208411356c18e17
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-02-21 16:01:20 -0800

    feat: add unit tests

--- src/token/token.service.spec.ts
+++ src/token/token.service.spec.ts
@@ -4,8 +4,11 @@ import { TokenService } from './token.service';
 
 describe('TokenService', () => {
   let service: TokenService;
+  const env = process.env;
 
-  beforeEach(async () => {
+  async function setupService(env: any) {
+    jest.resetModules();
+    process.env = { ...env };
     const module: TestingModule = await Test.createTestingModule({
       providers: [TokenService],
     })
@@ -13,9 +16,23 @@ describe('TokenService', () => {
       .compile();
 
     service = module.get<TokenService>(TokenService);
+  }
+
+  beforeEach(async () => {
+    return setupService(env);
+  });
+
+  afterEach(() => {
+    process.env = env;
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('hasValidToken should return true with valid token', async () => {
+    expect(service.hasValidToken()).toBe(false);
+    await setupService({ ...env, BROKER_TOKEN: 'token' });
+    expect(service.hasValidToken()).toBe(true);
   });
 });
