644a07558c9d5f679bca28f411b4e68d1b522697
Matthew Bystedt <mbystedt@gmail.com>
2023-03-15 10:47:35 -0700

    feat: add unit tests (#57)
    * feat: add unit tests
    * feat: add health controller test

--- src/health/health.service.spec.ts
+++ src/health/health.service.spec.ts
@@ -1,21 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { createMock } from '@golevelup/ts-jest';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { HealthService } from './health.service';
+import { TokenService } from '../token/token.service';
 
 describe('HealthService', () => {
   let service: HealthService;
+  let tokenService: DeepMocked<TokenService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HealthService],
-    })
-      .useMocker(createMock)
-      .compile();
+      providers: [
+        HealthService,
+        TokenService,
+        {
+          provide: TokenService,
+          useValue: createMock<TokenService>(),
+        },
+      ],
+    }).compile();
 
     service = module.get<HealthService>(HealthService);
+    tokenService = module.get(TokenService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('check should return true if there is a valid token', () => {
+    tokenService.hasValidToken.mockReturnValue(true);
+    expect(service.check()).toBe(true);
+  });
+
+  it('check should throw error if there is no valid token', () => {
+    tokenService.hasValidToken.mockReturnValue(false);
+    expect(() => {
+      service.check();
+    }).toThrow();
   });
 });
