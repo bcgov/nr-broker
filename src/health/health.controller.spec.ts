cea5f1fa7bb30fdee17d69fe412e8426e6298086
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-03-15 10:43:55 -0700

    feat: add health controller test

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,9 +1,12 @@
-import { createMock } from '@golevelup/ts-jest';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
 
 describe('HealthController', () => {
   let controller: HealthController;
+  // let healthCheckService: DeepMocked<HealthCheckService>;
+  let healthService: DeepMocked<HealthService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +16,15 @@ describe('HealthController', () => {
       .compile();
 
     controller = module.get<HealthController>(HealthController);
+    healthService = module.get(HealthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('ping calls healthService.check()', () => {
+    controller.ping();
+    expect(healthService.check).toBeCalled();
   });
 });
