e67e0f3936261c098a359eac7e47663745de8b6d
GraceRuan <grace.ruan@gov.bc.ca>
2023-05-20 05:54:07 -0700

    feat: update catch error and subscribe method

--- ui/src/app/healthstatus.service.ts
+++ ui/src/app/healthstatus.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,11 @@ export class HealthstatusService {
       {
         responseType: 'json',
       },
+    ).pipe(
+      catchError((error)=>{
+        console.log("service fail",error);
+        throw error;
+      })
     );
   }
 
