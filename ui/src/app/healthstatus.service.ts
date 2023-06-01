668bcb515d244c12df69015255c5f09b46942d70
GraceRuan <grace.ruan@gov.bc.ca>
2023-05-31 17:10:23 -0700

    fix: add try catch block

--- ui/src/app/healthstatus.service.ts
+++ ui/src/app/healthstatus.service.ts
@@ -1,29 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HealthstatusService {
-
   constructor(private http: HttpClient) {}
 
-  healthCheck(){
-    return this.http.get<any>(`${environment.apiUrl}/v1/health`,
-      {
-        responseType: 'json',
-      },
-    ).pipe(
-      catchError((error)=>{
-        console.log("service fail",error);
-        throw error;
-      })
-    );
+  healthCheck() {
+    return this.http.get<any>(`${environment.apiUrl}/v1/health`);
   }
-
-
-
 }
