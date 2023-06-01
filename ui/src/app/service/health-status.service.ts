f67d33a02c360c3c1041cfa5ab93571beca857f3
Matthew Bystedt <mbystedt@gmail.com>
2023-06-01 16:20:33 -0700

    Feat/add health2 (#78)
    * feat: add health status in toolbar
    
    * feat: update catch error and subscribe method
    
    * fix: add try catch block
    
    * fix: move service to new location
    
    ---------
    
    Co-authored-by: GraceRuan <grace.ruan@gov.bc.ca>

---
+++ ui/src/app/service/health-status.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HealthStatusService {
+  constructor(private http: HttpClient) {}
+
+  healthCheck() {
+    return this.http.get<any>(`${environment.apiUrl}/v1/health`);
+  }
+}
--- ui/src/app/toolbar/toolbar.component.ts
+++ ui/src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  EventEmitter,
+  Inject,
+  Output,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,6 +17,9 @@ import { CURRENT_USER } from '../app-initialize.factory';
 import { UserDto } from '../service/graph.types';
 import { environment } from '../../environments/environment';
 import { RolesDialogComponent } from './roles-dialog/roles-dialog.component';
+import { HealthStatusService } from '../service/health-status.service';
+import { interval, Subject } from 'rxjs';
+import { takeUntil, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-toolbar',
@@ -21,15 +32,69 @@ import { RolesDialogComponent } from './roles-dialog/roles-dialog.component';
     MatIconModule,
     MatMenuModule,
     MatDividerModule,
+    CommonModule,
   ],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit, OnDestroy {
   @Output() sidebarClick = new EventEmitter<boolean>();
 
   constructor(
     private dialog: MatDialog,
+    private healthService: HealthStatusService,
     @Inject(CURRENT_USER) public user: UserDto,
   ) {}
+
+  healthStatus: boolean | undefined;
+  private unsubscribe = new Subject<void>();
+  isHovered: boolean | undefined;
+  statusText: string | undefined;
+
+  ngOnInit(): void {
+    try {
+      interval(5000)
+        .pipe(takeUntil(this.unsubscribe))
+        .subscribe(() => {
+          this.getHealthCheck();
+        });
+    } catch (error: any) {
+      this.healthStatus = false;
+    }
+
+    // Initial health check
+    this.getHealthCheck();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  getHealthCheck(): any {
+    try {
+      this.healthService
+        .healthCheck()
+        .pipe(
+          catchError((error: any) => {
+            this.healthStatus = false;
+            throw error;
+          }),
+        )
+        .subscribe((data: any) => {
+          if (data === null) {
+            this.healthStatus = false;
+          } else {
+            this.healthStatus = data.status === 'ok';
+          }
+        });
+    } catch (error: any) {
+      this.healthStatus = false;
+    }
+  }
+
+  showStatusText(isHovered: boolean) {
+    this.isHovered = isHovered;
+    this.statusText = this.healthStatus ? 'Online' : 'Offline';
+  }
 
   showRolesDialog() {
     this.dialog.open(RolesDialogComponent, {
