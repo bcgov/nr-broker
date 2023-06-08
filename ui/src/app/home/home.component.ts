4d149067b64aec0a3f3338fd0e02f69a2342786f
Nisarg <nisargvv@gmail.com>
2023-06-08 14:27:03 -0700

    Add external services cards

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, RouterModule, MatCardModule, MatDividerModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
