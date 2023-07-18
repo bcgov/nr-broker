c25b9e8576bbb95d790aaebb9322c2a7249fc33c
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-07-18 16:26:55 -0700

    feat: Use vault db engine to run locally with non-root account

---
+++ scripts/db/mongo-reset-admin-pass.js
@@ -0,0 +1,4 @@
+use("admin");
+if (db.getUser("admin_db_engine") !== null) {
+  db.changeUserPassword("admin_db_engine", "admin_secret");
+}
--- scripts/setenv-backend-dev.sh
+++ scripts/setenv-backend-dev.sh
@@ -21,6 +21,7 @@ export VAULT_TOKEN=$(eval $VAULT_TOKEN_CMD)
 BROKER_ROLE_ID=$(vault read -format json auth/$VAULT_APPROLE_PATH/role/$VAULT_BROKER_ROLE/role-id | jq -r '.data.role_id')
 BROKER_SECRET_ID=$(vault write -format json -f auth/$VAULT_APPROLE_PATH/role/$VAULT_BROKER_ROLE/secret-id | jq -r '.data.secret_id')
 export BROKER_TOKEN=$(vault write -format json -f auth/$VAULT_APPROLE_PATH/login role_id=$BROKER_ROLE_ID secret_id=$BROKER_SECRET_ID | jq -r '.auth.client_token')
+MONGODB_AUTH=$(vault read -format json database/creds/broker-role)
 export VAULT_TOKEN=""
 if [ -n "$1" ] && [ "kinesis" = "$1" ]; then
   export VAULT_TOKEN=$AUDIT_VAULT_TOKEN
@@ -45,5 +46,5 @@ export OAUTH2_CLIENT_REGISTRATION_LOGIN_POST_LOGOUT_REDIRECT_URI=http://localhos
 export OAUTH2_CLIENT_SESSION_SECRET=super+secret+session+key
 
 export MONGODB_URL="mongodb://{{username}}:{{password}}@localhost:27017/brokerDB?authSource=admin"
-export MONGODB_USERNAME="mongoadmin"
-export MONGODB_PASSWORD="secret"
+export MONGODB_USERNAME=$(echo $MONGODB_AUTH | jq -r '.data.username')
+export MONGODB_PASSWORD=$(echo $MONGODB_AUTH | jq -r '.data.password')
