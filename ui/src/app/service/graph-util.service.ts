ac7052d50342c34beae82521ddf641d4ffea47e2
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-09-21 16:12:45 -0700

    feat: ui improvments

--- ui/src/app/service/graph-util.service.ts
+++ ui/src/app/service/graph-util.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { GraphDataResponseEdgeDto } from './dto/graph-data.dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GraphUtilService {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+  constructor(private readonly router: Router) {}
 
   snakecase(str: string) {
     return str.replace(
@@ -17,5 +17,22 @@ export class GraphUtilService {
 
   edgeToMapString(e: Pick<GraphDataResponseEdgeDto, 'is' | 'it' | 'name'>) {
     return `${e.is}>${e.it}:${e.name}`;
+  }
+
+  openInGraph(id: string, type: 'edge' | 'vertex') {
+    this.router.navigate(
+      [
+        '/graph',
+        {
+          selected: JSON.stringify({
+            id,
+            type,
+          }),
+        },
+      ],
+      {
+        replaceUrl: true,
+      },
+    );
   }
 }
