dd685fe267cb34356f0bb69b716f4b5c730b0d24
Matthew Bystedt <mbystedt@gmail.com>
2024-05-23 15:17:08 -0700

    fix: add OIDC guard to connection api

--- src/system/system.controller.ts
+++ src/system/system.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { SystemService } from './system.service';
+import { BrokerOidcAuthGuard } from '../auth/broker-oidc-auth.guard';
 
 @Controller({
   path: 'system',
@@ -9,6 +10,7 @@ export class SystemController {
   constructor(private readonly systemService: SystemService) {}
 
   @Get('preference/connection')
+  @UseGuards(BrokerOidcAuthGuard)
   getConnections() {
     return this.systemService.getConnections();
   }
