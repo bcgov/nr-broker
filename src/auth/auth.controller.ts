2316d3f168ad54bc0380cbb18731d3f6b78dc748
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2024-07-17 15:55:27 -0700

    fix: logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -32,24 +32,25 @@ export class AuthController {
   async logout(@Request() req: ExpressRequest, @Res() res: Response) {
     const id_token = req.user ? (req.user as any).id_token : undefined;
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    req.logout(() => {});
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    req.session.destroy(async (error: any) => {
-      const TrustIssuer = await Issuer.discover(
-        `${process.env.OAUTH2_CLIENT_PROVIDER_OIDC_ISSUER}/.well-known/openid-configuration`,
-      );
-      const end_session_endpoint = TrustIssuer.metadata.end_session_endpoint;
-      if (end_session_endpoint) {
-        res.redirect(
-          end_session_endpoint +
-            '?post_logout_redirect_uri=' +
-            process.env
-              .OAUTH2_CLIENT_REGISTRATION_LOGIN_POST_LOGOUT_REDIRECT_URI +
-            (id_token ? '&id_token_hint=' + id_token : ''),
+    req.logout(() => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      req.session.destroy(async (error: any) => {
+        const TrustIssuer = await Issuer.discover(
+          `${process.env.OAUTH2_CLIENT_PROVIDER_OIDC_ISSUER}/.well-known/openid-configuration`,
         );
-      } else {
-        res.redirect('/');
-      }
+        const end_session_endpoint = TrustIssuer.metadata.end_session_endpoint;
+        if (end_session_endpoint) {
+          res.redirect(
+            end_session_endpoint +
+              '?post_logout_redirect_uri=' +
+              process.env
+                .OAUTH2_CLIENT_REGISTRATION_LOGIN_POST_LOGOUT_REDIRECT_URI +
+              (id_token ? '&id_token_hint=' + id_token : ''),
+          );
+        } else {
+          res.redirect('/');
+        }
+      });
     });
   }
 }
