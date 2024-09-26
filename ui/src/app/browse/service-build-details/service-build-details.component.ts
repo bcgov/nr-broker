0839203fc86f491b860a63d3e09bcb5e26f5fd6d
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2024-09-25 17:01:16 -0700

    fix: ui margins and add open scm button

--- ui/src/app/browse/service-build-details/service-build-details.component.ts
+++ ui/src/app/browse/service-build-details/service-build-details.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { combineLatest } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { combineLatest, Subject, takeUntil } from 'rxjs';
 
 import { CollectionHeaderComponent } from '../../shared/collection-header/collection-header.component';
 import { PackageApiService } from '../../service/package-api.service';
@@ -12,22 +15,29 @@ import { PackageBuildRestDto } from '../../service/dto/package-build-rest.dto';
 import { FilesizePipe } from '../../util/filesize.pipe';
 import { CollectionNames } from '../../service/dto/collection-dto-union.type';
 import { CollectionApiService } from '../../service/collection-api.service';
+import { PackageUtilService } from '../../service/package-util.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-service-build-details',
   standalone: true,
   imports: [
     CommonModule,
+    ClipboardModule,
     CollectionHeaderComponent,
     FilesizePipe,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTooltipModule,
     MatProgressSpinnerModule,
   ],
   templateUrl: './service-build-details.component.html',
   styleUrl: './service-build-details.component.scss',
 })
-export class ServiceBuildDetailsComponent implements OnInit {
+export class ServiceBuildDetailsComponent implements OnInit, OnDestroy {
   collection!: CollectionNames;
   serviceId!: string;
   buildId!: string;
@@ -36,14 +46,43 @@ export class ServiceBuildDetailsComponent implements OnInit {
   hasDelete = false;
   screenSize = 'wide';
 
+  // Create a map from breakpoints to css class
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'narrow'],
+    [Breakpoints.Small, 'narrow'],
+    [Breakpoints.Medium, 'wide'],
+    [Breakpoints.Large, 'wide'],
+    [Breakpoints.XLarge, 'wide'],
+  ]);
+
   loading = true;
   data: PackageBuildRestDto | undefined;
+
+  private ngUnsubscribe: Subject<any> = new Subject();
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
     private readonly collectionApi: CollectionApiService,
     private readonly packageApi: PackageApiService,
-  ) {}
+    private readonly packageUtil: PackageUtilService,
+  ) {
+    inject(BreakpointObserver)
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((result) => {
+        for (const query of Object.keys(result.breakpoints)) {
+          if (result.breakpoints[query]) {
+            this.screenSize = this.displayNameMap.get(query) ?? 'Unknown';
+          }
+        }
+      });
+  }
 
   ngOnInit(): void {
     const params = this.activatedRoute.snapshot.params;
@@ -59,6 +98,14 @@ export class ServiceBuildDetailsComponent implements OnInit {
       this.data = data;
       this.loading = false;
     });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.complete();
+  }
+
+  async openPackageBuildVersion(id: string, version: string) {
+    return this.packageUtil.openPackageBuildVersion(id, version);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
