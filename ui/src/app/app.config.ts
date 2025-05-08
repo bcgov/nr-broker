c37cbdf45e2f536c77a089de011ccf80da735fe3
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2025-05-08 11:59:48 -0700

    chore: update application bootstrap to follow new standard

---
+++ ui/src/app/app.config.ts
@@ -0,0 +1,49 @@
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+} from '@angular/core';
+import { provideRouter, TitleStrategy } from '@angular/router';
+import {
+  HttpClient,
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes';
+import { RouteTitleStrategy } from './route-title.strategy';
+import {
+  appInitializePrefFactory,
+  appInitializeUserFactory,
+  appInitializeConfigFactory,
+} from './app-initialize.factory';
+import { AuthInterceptor } from './auth.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideAppInitializer(() => {
+      const http = inject(HttpClient);
+      return appInitializePrefFactory(http);
+    }),
+    provideAppInitializer(() => {
+      const http = inject(HttpClient);
+      return appInitializeUserFactory(http);
+    }),
+    provideAppInitializer(() => {
+      const http = inject(HttpClient);
+      return appInitializeConfigFactory(http);
+    }),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {
+      provide: TitleStrategy,
+      useClass: RouteTitleStrategy,
+    },
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideNativeDateAdapter(),
+  ],
+};
