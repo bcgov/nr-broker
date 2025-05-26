0d8011f96e2ae14d8833964345cc97eb40f26d49
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2025-05-26 10:06:33 -0700

    chore: update to input()

--- ui/src/app/graph/vertex-name/vertex-name.component.ts
+++ ui/src/app/graph/vertex-name/vertex-name.component.ts
@@ -1,22 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 import { GraphDataVertex } from '../../service/graph.types';
 import { GraphTypeaheadData } from '../../service/graph/dto/graph-typeahead-result.dto';
 
 @Component({
   selector: 'app-vertex-name',
-  template: `@if (vertex) {
-    @if (vertex.parentName) {
-      {{ vertex.parentName }} &gt;
+  template: `@if (vertex().parentName) {
+      {{ vertex().parentName }} &gt;
     }
-    {{ vertex.name }}
-  }`,
+    {{ vertex().name }}`,
   styles: [],
   imports: [],
 })
 export class VertexNameComponent {
-  // TODO: Skipped for migration because:
-  //  This input is used in a control flow expression (e.g. `@if` or `*ngIf`)
-  //  and migrating would break narrowing currently.
-  @Input() vertex: GraphDataVertex | GraphTypeaheadData | undefined;
+  readonly vertex = input.required<GraphDataVertex | GraphTypeaheadData>();
 }
