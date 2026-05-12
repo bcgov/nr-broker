80ec5eafc47933882207c133942afb766bda1ca1
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2026-05-12 16:54:10 -0700

    fix: linting

--- api/src/collection/dto/package-build-search-query.dto.ts
+++ api/src/collection/dto/package-build-search-query.dto.ts
@@ -1,5 +1,5 @@
-import { Type } from "class-transformer";
-import { IsInt, IsOptional, IsString } from "class-validator";
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, IsString } from 'class-validator';
 
 export class PackageBuildSearchQuery {
   @IsOptional()
